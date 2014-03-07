448518fcfc7a22279f814608fbfaf7f6b28108f2
Caleb <hktan_MBP@sarahsmacbook2.lan>
2014-03-07 08:29:11 +0100

    added Table View Controller

---
+++ UITableViewController Challenge Solution/CTTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  CTTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Caleb on 05/03/14.
+//  Copyright (c) 2014 Caleb Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *firstArray;
+@property (strong, nonatomic) NSMutableArray *secondArray;
+
+
+@end
---
+++ UITableViewController Challenge Solution/CTTableViewController.m
@@ -0,0 +1,147 @@
+//
+//  CTTableViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Caleb on 05/03/14.
+//  Copyright (c) 2014 Caleb Tan. All rights reserved.
+//
+
+#import "CTTableViewController.h"
+
+@interface CTTableViewController ()
+
+@end
+
+@implementation CTTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    self.firstArray = [[NSMutableArray alloc] init];
+    self.secondArray = [[NSMutableArray alloc] init];
+    NSString *firstString = @"I am in section 0";
+    NSString *secondString = @"another section";
+    
+    [self.firstArray addObject:firstString];
+    [self.firstArray addObject:firstString];
+    [self.secondArray addObject:secondString];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 3;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    if (section == 0) {
+        return [self.firstArray count];
+    }
+    else if (section == 1){
+        return [self.secondArray count];
+    }
+    else {
+    return 3;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    if (indexPath.section == 0) {
+        cell.textLabel.text = [self.firstArray objectAtIndex:indexPath.row];
+        cell.backgroundColor = [UIColor redColor];
+    }
+    else if (indexPath.section == 1){
+        cell.textLabel.text = [self.secondArray objectAtIndex:indexPath.row];
+        cell.backgroundColor = [UIColor blueColor];
+    }
+    else{
+     cell.textLabel.text = [NSString stringWithFormat:@"Row %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
+    }
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+*/
+
+@end
